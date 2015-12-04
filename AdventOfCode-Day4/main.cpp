720e13b4840b780489d941d434280e4a7cb4661c
SPARKY892 <marksmellie892@gmail.com>
2015-12-04 10:30:27 +0000

    Added code to increase number and add to string then check for zeros

--- AdventOfCode-Day4/main.cpp
+++ AdventOfCode-Day4/main.cpp
@@ -1,18 +1,37 @@
 #include "md5.h"
 #include<iostream>>
 #include<string>
+#include<sstream>
 
 using namespace std;
 
 int main()
 {
 	string input = "iwrupvqb";
+	string md5Output;
+	ostringstream oss;
 
 	int number = 0;
 	bool found = false;
 
 	while (!found)
 	{
-		md5()
+		oss << number;
+		input += oss.str();
+
+		md5Output = md5(input);
+
+		if (md5Output.substr(0,3) == "00000")
+		{
+			found = true;
+			cout << number << endl;
+		}
+		else {
+			found = false;
+			number++;
+		}
 	}
+
+	cin.get();
+	return 0;	
 }
