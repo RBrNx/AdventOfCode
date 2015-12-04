edf0345bd25e8350837ba2fcb11f34f85adea5d9
SPARKY892 <marksmellie892@gmail.com>
2015-12-04 09:46:32 +0000

    Added Day 4

---
+++ AdventOfCode-Day4/main.cpp
@@ -0,0 +1,18 @@
+#include "md5.h"
+#include<iostream>>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	string input = "iwrupvqb";
+
+	int number = 0;
+	bool found = false;
+
+	while (!found)
+	{
+		md5()
+	}
+}
