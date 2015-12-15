93f86f7d609415efeb8dcd3e1c98f14ede517c5d
RBrNx277 <b00231929@studentmail.uws.ac.uk>
2015-12-15 15:47:11 +0000

    Day 14 - Part 1 and 2 working!

--- AdventOfCode-Day14/Source.cpp
+++ AdventOfCode-Day14/Source.cpp
@@ -8,12 +8,19 @@ using namespace std;
 
 vector<string> lines;
 string line;
+int furthestDistance = 0;
+int mostPoints = 0;
+string winningReindeer;
 
 struct reindeer {
 	string name;
 	int speed;
-	int flyTime;
-	int restTime;
+	int defaultFlyTime;
+	int defaultRestTime;
+	int flightTime;
+	int restingTime;
+	int distanceTravelled;
+	int points;
 };
 
 vector<struct reindeer> reindeers;
@@ -43,28 +50,73 @@ int main()
 	for (int i = 0; i < lines.size(); i++)
 	{
 		string name = lines[i].substr(0, lines[i].find(' '));
-		lines[i].erase(0, lines[i].find(' ') + 8);
-		int speed = stoi(lines[i].substr(0, lines[i].find(' ')), NULL, 10);
 		lines[i].erase(0, lines[i].find(' ') + 9);
+		int speed = stoi(lines[i].substr(0, lines[i].find(' ')), NULL, 10);
+		lines[i].erase(0, lines[i].find(' ') + 10);
 		int flyTime = stoi(lines[i].substr(0, lines[i].find(' ')), NULL, 10);
-		lines[i].erase(0, lines[i].find(' ') + 32);
+		lines[i].erase(0, lines[i].find(' ') + 33);
 		int restTime = stoi(lines[i].substr(0, lines[i].find(' ')), NULL, 10);
 
 		reindeer Reindeer;
 		Reindeer.name = name;
 		Reindeer.speed = speed;
-		Reindeer.flyTime = flyTime;
-		Reindeer.restTime = restTime;
+		Reindeer.flightTime = flyTime;
+		Reindeer.restingTime = restTime;
+		Reindeer.defaultFlyTime = flyTime;
+		Reindeer.defaultRestTime = restTime;
+		Reindeer.distanceTravelled = 0;
+		Reindeer.points = 0;
 
 		reindeers.push_back(Reindeer);
 	}
 
 	while (seconds > 0)
 	{
-
-
-
-
+		for (int i = 0; i < reindeers.size(); i++) {
+			if (reindeers[i].flightTime > 0) {
+				reindeers[i].flightTime--;
+				reindeers[i].distanceTravelled += reindeers[i].speed;
+			}
+			else {
+				if (reindeers[i].restingTime > 0) reindeers[i].restingTime--;
+				else {
+					reindeers[i].restingTime = reindeers[i].defaultRestTime;
+					reindeers[i].flightTime = reindeers[i].defaultFlyTime;
+					reindeers[i].flightTime--;
+					reindeers[i].distanceTravelled += reindeers[i].speed;
+				}
+			}
+		}
+		for (int i = 0; i < reindeers.size(); i++) {
+			if (reindeers[i].distanceTravelled > furthestDistance) {
+				furthestDistance = reindeers[i].distanceTravelled;
+			}
+		}
+		for (int i = 0; i < reindeers.size(); i++) {
+			if (reindeers[i].distanceTravelled == furthestDistance) {
+				reindeers[i].points++;
+			}
+		}
 		seconds--;
 	}
+
+	furthestDistance = 0;
+	for(int i = 0; i < reindeers.size(); i++) {
+		if (reindeers[i].distanceTravelled > furthestDistance) {
+			furthestDistance = reindeers[i].distanceTravelled;
+			winningReindeer = reindeers[i].name;
+		}
+	}
+	cout << winningReindeer << "- Furthest Distance: " << furthestDistance << endl;
+
+	for (int i = 0; i < reindeers.size(); i++) {
+		if (reindeers[i].points > mostPoints) {
+			mostPoints = reindeers[i].points;
+			winningReindeer = reindeers[i].name;
+		}
+	}
+	cout << winningReindeer << "- Most Points: " << mostPoints << endl;
+
+	
+	cin.get();
 }
