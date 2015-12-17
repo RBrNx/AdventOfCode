0970f52dd7abb0e20cb3107b1c187475b6d7b86f
RBrNx277 <b00231929@studentmail.uws.ac.uk>
2015-12-17 17:17:08 +0000

    Added Code for Day 15 - not working yet

--- AdventOfCode-Day15/Source.cpp
+++ AdventOfCode-Day15/Source.cpp
@@ -6,38 +6,135 @@
 
 using namespace std;
 
-void swap(int places[], int i, int j) {
-	int temp = 0;
-	temp = places[i];
-	places[i] = places[j];
-	places[j] = temp;
+vector<string> lines;
+string line;
+
+void readFile(string filename) {
+	ifstream myFile(filename);
+
+	if (myFile.is_open())
+	{
+		while (getline(myFile, line))
+		{
+			lines.push_back(line);
+		}
+		myFile.close();
+	}
+	else
+	{
+		cout << "Unable to open file";
+	}
 }
 
-void perm(int combo[], int n, int i) {
+struct bestCookie {
+	int spoonfulsIngredient1;
+	int spoonfulsIngredient2;
+	int spoonfulsIngredient3;
+	int spoonfulsIngredient4;
+	int highestScore = 0;
+} BestCookie;
+
+struct ingredient {
+	string name;
+	int capacity;
+	int durability;
+	int flavour;
+	int texture;
+	int calories;
+	int spoonfuls;
+};
+
+vector<struct ingredient> ingredients;
+
+int CapacityScore;
+int DurabilityScore;
+int FlavourScore;
+int TextureScore;
+
+void swap(vector<struct ingredient> ingredients, int i, int j) {
+	struct ingredient temp;
+	temp = ingredients[i];
+	ingredients[i] = ingredients[j];
+	ingredients[j] = temp;
+}
+
+void perm(vector<struct ingredient> ingredients, int n, int i) {
 	if (i == n) {
-		for (int i = 0; i < 4; i++) {
-			cout << combo[i] << "  ";
+		if ((CapacityScore = (ingredients[0].spoonfuls * ingredients[0].capacity) + (ingredients[1].spoonfuls * ingredients[1].capacity) + (ingredients[2].spoonfuls * ingredients[2].capacity) + (ingredients[3].spoonfuls * ingredients[3].capacity)) < 0) { CapacityScore = 0; }
+		if ((DurabilityScore = (ingredients[0].spoonfuls * ingredients[0].durability) + (ingredients[1].spoonfuls * ingredients[1].durability) + (ingredients[2].spoonfuls * ingredients[2].durability) + (ingredients[3].spoonfuls * ingredients[3].durability)) < 0) { DurabilityScore = 0; }
+		if ((FlavourScore = (ingredients[0].spoonfuls * ingredients[0].flavour) + (ingredients[1].spoonfuls * ingredients[1].flavour) + (ingredients[2].spoonfuls * ingredients[2].flavour) + (ingredients[3].spoonfuls * ingredients[3].flavour)) < 0) { FlavourScore = 0; }
+		if ((TextureScore = (ingredients[0].spoonfuls * ingredients[0].texture) + (ingredients[1].spoonfuls * ingredients[1].texture) + (ingredients[2].spoonfuls * ingredients[2].texture) + (ingredients[3].spoonfuls * ingredients[3].texture)) < 0) { TextureScore = 0; }
+
+		int totalScore = CapacityScore * DurabilityScore * FlavourScore * TextureScore;
+
+		if (totalScore > BestCookie.highestScore) {
+			BestCookie.highestScore = totalScore;
+			BestCookie.spoonfulsIngredient1 = ingredients[0].spoonfuls;
+			BestCookie.spoonfulsIngredient2 = ingredients[1].spoonfuls;
+			BestCookie.spoonfulsIngredient3 = ingredients[2].spoonfuls;
+			BestCookie.spoonfulsIngredient4 = ingredients[3].spoonfuls;
+
 		}
-		cout << endl;
 	}
 	else {
 		for (int j = i; j < n; j++) {
-			swap(combo, i, j);
-			perm(combo, n, i + 1);
-			swap(combo, i, j);
+			swap(ingredients, i, j);
+			perm(ingredients, n, i + 1);
+			swap(ingredients, i, j);
 		}
 	}
 }
 
 int main() {
-	int ints[4];
+	readFile("data.txt");
+	for (int i = 0; i < lines.size(); i++) {
+		string Name = lines[i].substr(0, lines[i].find(':'));
+		lines[i].erase(0, Name.length() + 11); //Erases Name + ": " + Capacity
 
-	for (int i = 0; i < 4; i++) {
-		ints[i] = i;
+		int Capacity = stoi(lines[i].substr(0, lines[i].find(',')), NULL, 10);
+		lines[i].erase(0, lines[i].find(',') + 13); //Erases Capacity value and "Durability "
+
+		int Durability = stoi(lines[i].substr(0, lines[i].find(',')), NULL, 10);
+		lines[i].erase(0, lines[i].find(',') + 9); //Erases Durability value and "Flavour "
+
+		int Flavour = stoi(lines[i].substr(0, lines[i].find(',')), NULL, 10);
+		lines[i].erase(0, lines[i].find(',') + 10); //Erases Flavour value and "Texture "
+
+		int Texture = stoi(lines[i].substr(0, lines[i].find(',')), NULL, 10);
+		lines[i].erase(0, lines[i].find(',') + 11); //Erases Texture value and "Calories "
+
+		int Calories = stoi(lines[i].substr(0, lines[i].find(',')), NULL, 10);
+		lines[i].erase(0, lines[i].find(',') + 2); //Erases Calories value
+
+		struct ingredient Ingredient;
+
+		Ingredient.name = Name;
+		Ingredient.capacity = Capacity;
+		Ingredient.durability = Durability;
+		Ingredient.flavour = Flavour;
+		Ingredient.texture = Texture;
+		Ingredient.calories = Calories;
+
+		ingredients.push_back(Ingredient);
 	}
 
-	perm(ints, 4, 0);
+	ingredients[0].spoonfuls = 1, ingredients[1].spoonfuls = 1, ingredients[2].spoonfuls = 1, ingredients[3].spoonfuls = 97;
 
-	cout << "Program Finshed";
-	cin.get();
+	while (ingredients[3].spoonfuls != 1) {
+		perm(ingredients, 3, 0);
+		ingredients[0].spoonfuls++;
+		ingredients[3].spoonfuls = 100 - ingredients[0].spoonfuls - ingredients[1].spoonfuls - ingredients[2].spoonfuls;
+		perm(ingredients, 3, 0);
+		ingredients[1].spoonfuls++;
+		ingredients[3].spoonfuls = 100 - ingredients[0].spoonfuls - ingredients[1].spoonfuls - ingredients[2].spoonfuls;
+		perm(ingredients, 3, 0);
+		ingredients[2].spoonfuls++;
+		ingredients[3].spoonfuls = 100 - ingredients[0].spoonfuls - ingredients[1].spoonfuls - ingredients[2].spoonfuls;
+	}
+
+	std::cout << "Score: " << BestCookie.highestScore << endl;;
+	std::cout << BestCookie.spoonfulsIngredient1 << " Spoonfuls of Frosting, " << BestCookie.spoonfulsIngredient2 << " Spoonfuls of Candy, " << BestCookie.spoonfulsIngredient3 << " Spoonfuls of Butterscotch, " << BestCookie.spoonfulsIngredient4 << " Spoonfuls of Sugar" << endl;
+
+
+	std::cin.get();
 }
