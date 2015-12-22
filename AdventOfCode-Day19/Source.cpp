32ddf3b868906e50ab30ad94d2235f54b8bcc26b
RBrNx277 <b00231929@studentmail.uws.ac.uk>
2015-12-22 23:04:00 +0000

    Added Day 19 - Part 1 and 2.

--- AdventOfCode-Day19/Source.cpp
+++ AdventOfCode-Day19/Source.cpp
@@ -1,33 +1,95 @@
+//THIS IS NOT MY CODE - I COULD NOT COMPLETE THIS PUZZLE
+
 #include <iostream>
-#include <string>
 #include <fstream>
-#include <vector>
-#include <algorithm>
+#include <string>
+#include <regex>
+#include <map>
 
-using namespace std;
+using Reps = std::multimap<std::string, std::string>;
 
-vector<string> lines;
-string line;
+Reps readInput(std::string& molecule)
+{
+	std::ifstream infile("data.txt");
+	std::string line;
+	std::regex rex("(\\w+) => (\\w+)");
+	std::smatch match;
+	Reps replacements;
 
-void readFile(string filename) {
-	ifstream myFile(filename);
-
-	if (myFile.is_open())
+	while (std::getline(infile, line))
 	{
-		while (getline(myFile, line))
+		if (std::regex_match(line, match, rex))
 		{
-			lines.push_back(line);
+			if (match.size() < 3)
+				return replacements;
+			replacements.insert(std::make_pair(match[1], match[2]));
 		}
-		myFile.close();
+		else {
+			molecule = line;
+		}
 	}
-	else
-	{
-		cout << "Unable to open file";
-	}
+	return replacements;
 }
 
-int main() {
-	readFile("data.txt");
+int partOne(Reps& replacements, std::string& molecule)
+{
+	std::map<std::string, int> newMolecules;
+	for (const auto& rep : replacements)
+	{
+		size_t pos = molecule.find(rep.first, 0);
+		while (pos != std::string::npos)
+		{
+			std::string mol = molecule;
+			mol.replace(pos, rep.first.length(), rep.second);
+			newMolecules[mol] = 1;
+			pos = molecule.find(rep.first, pos + 1);
+		}
+	}
+	return newMolecules.size();
+}
 
+int createMolecule(Reps& replacements, std::string& source)
+{
+	int steps = 0;
 
+	for (;;)
+	{
+		for (const auto& rep : replacements)
+		{
+			auto pos = source.find(rep.first, 0);
+			if (pos != std::string::npos)
+			{
+				source.replace(pos, rep.first.length(), rep.second);
+				steps++;
+			}
+			if (source == "e")
+				return steps;
+		}
+	}
+
+	return steps;
+}
+
+Reps reverseMap(Reps& source)
+{
+	Reps rest;
+	for (auto& element : source)
+	{
+		rest.insert(std::make_pair(element.second, element.first));
+	}
+	return rest;
+}
+
+int partTwo(Reps& replacements, std::string& molecule)
+{
+	return createMolecule(reverseMap(replacements), molecule);
+}
+
+int main()
+{
+	std::string molecule;
+	Reps replacements = readInput(molecule);
+	std::cout << "Part one: " << partOne(replacements, molecule) << std::endl;
+	std::cout << "Part two: " << partTwo(replacements, molecule) << std::endl;
+	std::cin.get();
 }
